f70072c6d4b170819df7afcc7bca1496205fa3f7
Yihao Zheng <yhzheng@ucdavis.edu>
2021-03-25 17:01:13 -0700

    SHINY

--- course_search/app.R
+++ course_search/app.R
@@ -2,6 +2,9 @@
 
 library(shiny)
 
+source("course_combination.R")
+
+
 ui <- fluidPage(
     
     titlePanel("Course Schedual Building Assistant"),
@@ -11,37 +14,88 @@ ui <- fluidPage(
             textInput(
                 inputId="course1",
                 label="Full Course Name:",
-                value="",
+                value=""
             ),
             textInput(
-                inputId="course1",
+                inputId="course2",
                 label="Full Course Name:",
-                value="",
+                value=""
             ),
             textInput(
-                inputId="course1",
+                inputId="course3",
                 label="Full Course Name:",
-                value="",
+                value=""
             ),
             textInput(
-                inputId="course1",
+                inputId="course4",
                 label="Full Course Name:",
-                value="",
+                value=""
+            ),
+            textInput(
+              inputId="course5",
+              label="Full Course Name:",
+              value=""
+            ),
+            textInput(
+              inputId="course6",
+              label="Full Course Name:",
+              value=""
+            ),
+            textInput(
+              inputId="course7",
+              label="Full Course Name:",
+              value=""
+            ),
+            textInput(
+              inputId="course8",
+              label="Full Course Name:",
+              value=""
             ),
             actionButton("do", "Click Me")
         ),
         
         mainPanel(
-            tableOutput("course_schedual")
+            textOutput("noclass"),
+            textOutput("course_schedual")
         )
     )
 )
 
 
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
+  input1<-toupper(input$course1)
+  input2<-toupper(input$course2)
+  input3<-toupper(input$course3)
+  input4<-toupper(input$course4)
+  input5<-toupper(input$course5)
+  input6<-toupper(input$course6)
+  input7<-toupper(input$course7)
+  input8<-toupper(input$course8)
+  input = c(input1,input2,input3,input4,input5,input6,input7,input8)
+  df_list = list()
+  course_num = 0
+  output$noclass<-renderText(
+    input$do,
+    for (i in input){
+      temp=df%>%filter(course_name == i)
+      if(i == ""){
+        next
+      }
+      if (nrow(temp) == 0){
+        cat(i)
+        print("NOT FOUND")
+      }
+    }
+  )
   
-})
+  output$course_schedual<-renderPrint(
+    input$do
+    
+  )
 }
 shinyApp(ui = ui, server = server)
+
+
+
+
+
